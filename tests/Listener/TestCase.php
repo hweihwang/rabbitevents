0519ee9219e7f882bffeed6d8dbf4d39e1334790
admin <admin@MacBook-Pro-cua-admin.local>
2022-06-06 20:22:10 +0700

    update package version

--- tests/Listener/TestCase.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace RabbitEvents\Tests\Listener;
-
-class TestCase extends \PHPUnit\Framework\TestCase
-{
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-}
--- tests/Publisher/TestCase.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace RabbitEvents\Tests\Publisher;
-
-class TestCase extends \PHPUnit\Framework\TestCase
-{
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-}
